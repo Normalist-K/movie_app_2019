ceaa014da1bca416b97e0aa82692b9cbf7d1c17a
Normalist <uvento06@hanmail.net>
2019-08-25 19:16:41 +0700

    Updates

---
+++ precache-manifest.cac364a443c2d13f753c1e7766a2d734.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67bd54b49b52220ca9ae382342f2c1c9",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "d0a79c9a3b86dd2753ef",
+    "url": "/movie_app_2019/static/css/main.cd5078d7.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "d0a79c9a3b86dd2753ef",
+    "url": "/movie_app_2019/static/js/main.8056659c.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
